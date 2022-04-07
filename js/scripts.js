558a96d001a658f8a80d95555e10eb53de885d35
Tukashov <erbol.tukashov.kk@gmail.com>
2022-04-08 03:06:31 +0600

    refactor: change page and sections and add first accordion

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
-$(function () {
+// NEED HELP PAGE SCRIPTS
+const accordionHeader = document.querySelectorAll('.programs-first-kids-hospice__inner__all-accordions__accordion__header');
 
-	$('.medical-programs__inner__blocks').slick({
-		dots: true,
-		infinite: true,
-		speed: 700,
-		slidesToShow: 1,
-		centerMode: false,
-		variableWidth: true
-	})
+accordionHeader.forEach(accordionHeader => {
+    accordionHeader.addEventListener('click', event => {
+        const currentActiveHeader = document.querySelector('.programs-first-kids-hospice__inner__all-accordions__accordion__header.active');
+        if (currentActiveHeader && currentActiveHeader!==accordionHeader) {
+            currentActiveHeader.classList.toggle('active');
+            currentActiveHeader.nextElementSibling.style.maxHeight = 0;
+        }
 
+        accordionHeader.classList.toggle('active');
+        const accordionBody = accordionHeader.nextElementSibling;
+
+        if (accordionHeader.classList.contains('active')) {
+            accordionBody.style.maxHeight = accordionBody.scrollHeight + 'px';
+        }
+        else {
+            accordionBody.style.maxHeight = 0;
+        }
+    });
 });
