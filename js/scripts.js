658c3331aad29bad4bfef605bede66a80d65b6d5
Tukashov <erbol.tukashov.kk@gmail.com>
2022-04-07 23:57:13 +0600

    feat: add slider

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(function () {
+
+	$('.medical-programs__inner__blocks').slick({
+		dots: true,
+		infinite: true,
+		speed: 700,
+		slidesToShow: 1,
+		centerMode: false,
+		variableWidth: true
+	})
+
+});
